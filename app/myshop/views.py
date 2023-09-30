946163db94b5f52c8dab1742c86a775e2d6e23ef
Nguyen-Vi <59189226+Vix-Nguyen@users.noreply.github.com>
2023-09-30 22:54:41 +0700

    [IMP] Refactor views

--- app/myshop/views.py
+++ app/myshop/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import get_object_or_404, render
 
 from .models import Product
 
 def index(request):
-    product_list = [product for product in Product.objects.order_by("id")]
+    product_list = Product.objects.all()
     template = loader.get_template("myshop/index.html")
     context = {
         "product_list": product_list
@@ -12,8 +13,8 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def product(request, product_id):
-    return HttpResponse("You're looking at product %s." % product_id)
-
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, "myshop/product.html", {"product": product})
 
 def results(request, product_id):
     response = "You're looking at the type of product_id %s."
