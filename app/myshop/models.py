d590848c9c088a5f0a43aec8ff991c3fcfde8a56
Nguyen-Vi <59189226+Vix-Nguyen@users.noreply.github.com>
2023-10-03 00:56:22 +0700

    [IMP] Refactor layout and product form

--- app/myshop/models.py
+++ app/myshop/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_length=200)
-    active = models.BooleanField(name='active')
-    price = models.PositiveIntegerField(default=0)
-    thumbnail = models.ImageField(upload_to='thumbnail/', default='thumbnail/default.png')
+    name = models.CharField(max_length=200, verbose_name="Name")
+    active = models.BooleanField(verbose_name="Active")
+    price = models.PositiveIntegerField(default=0, verbose_name='Price')
+    thumbnail = models.ImageField(upload_to='thumbnail/', default='thumbnail/default.png', verbose_name='Thumbnail')
+
+    def __str__(self):
+        return self.name
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
--- app/myshop/urls.py
+++ app/myshop/urls.py
@@ -8,5 +8,5 @@ app_name = "myshop"
 urlpatterns = [
     path("", views.index, name="index"),
     path("product/<int:product_id>/", views.product, name="product"),
-    path("product/new", views.create_product),
+    path("product/new", views.create_product, name='create_product'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
